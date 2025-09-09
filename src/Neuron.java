64f04a2aafe316833461dd12b4dbd9e68a7f5741
js12488 <broporg@gmail.com>
2025-09-08 23:07:19 -0400

    Added 3rd input for neuron

--- src/Neuron.java
+++ src/Neuron.java
@@ -4,9 +4,10 @@ public class Neuron { //For neurons
     Random randomDouble = new Random(); //for doubles
     public Double weightA = randomDouble.nextDouble(-1, 1); //new double from -1 to 1
     public Double weightB = randomDouble.nextDouble(-1, 1);
+    public Double weightC = randomDouble.nextDouble(-1, 1);
     public Double bias = randomDouble.nextDouble(-1, 1); //bias value
-    public double compute(double inputA, double inputB) { //compute the output of the neuron
-        double weightedSum = weightA * inputA + weightB * inputB; //weighted sum
+    public double compute(double inputA, double inputB, double inputC) { //compute the output of the neuron
+        double weightedSum = weightA * inputA + weightB * inputB + weightC * inputC; //weighted sum
         return 1/(1+Math.exp(-weightedSum)); //sigmoid activation function
 
     }
