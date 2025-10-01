2f252ded376020ea4c2049f62a2fb8290b4ac6ea
rubfiber <abhirkarnik@gmail.com>
2025-09-30 22:56:23 -0400

    mutate(), Remember(), forget()

--- src/Neuron.java
+++ src/Neuron.java
@@ -7,8 +7,37 @@ public class Neuron { //For neurons
     public Double weightC = randomDouble.nextDouble(-1, 1);
     public Double bias = randomDouble.nextDouble(-1, 1); //bias value
     public double compute(double inputA, double inputB, double inputC) { //compute the output of the neuron
-        double weightedSum = weightA * inputA + weightB * inputB + weightC * inputC; //weighted sum
+        double weightedSum = weightA * inputA + weightB * inputB + weightC * inputC + bias;//weighted sum
         return 1/(1+Math.exp(-weightedSum)); //sigmoid activation function
 
+    }
+    public double newWeightA = randomDouble.nextDouble(-1, 1);
+    public double newWeightB = randomDouble.nextDouble(-1, 1);
+    public double newWeightC = randomDouble.nextDouble(-1, 1);
+    public double newBias = randomDouble.nextDouble(-1, 1);
+    public void mutate() {
+        int mutateProperty = randomDouble.nextInt(0, 5);
+        double factorOfChange = randomDouble.nextDouble(-1, 1);
+        if (mutateProperty == 0) {
+            newBias += factorOfChange;
+        } else if (mutateProperty == 1) {
+            newWeightA += factorOfChange;
+        } else if (mutateProperty == 2) {
+            newWeightB += factorOfChange;
+        } else {
+            newWeightC += factorOfChange;
+        }
+    }
+    public void forget() {
+        newBias = bias;
+        newWeightA = weightA;
+        newWeightB = weightB;
+        newWeightC = weightC;
+    }
+    public void Remember() {
+        bias = newBias;
+        weightA = newWeightA;
+        weightB = newWeightB;
+        weightC = newWeightC;
     }
 }
