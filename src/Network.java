605e95632ce07cd9216cb145e60cb05da3154af6
rubfiber <abhirkarnik@gmail.com>
2025-09-30 22:55:30 -0400

    Added train() method

--- src/Network.java
+++ src/Network.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -25,7 +26,7 @@ class Network {
         double n7 = NeuronNetwork.get(7).compute(n3, n4, n5);
         double n8 = NeuronNetwork.get(8).compute(n3, n4, n5);
 
-// Layer 4 (assuming 2 neurons here: 9 and 10)
+// Layer 4
         double n9 = NeuronNetwork.get(9).compute(n6, n7, n8);
         double n10 = NeuronNetwork.get(10).compute(n6, n7, n8);
         double n11 = NeuronNetwork.get(11).compute(n6, n7, n8);
@@ -33,16 +34,42 @@ class Network {
 // Output
         return Arrays.asList(NeuronNetwork.get(12).compute(n9, n10, n11), NeuronNetwork.get(13).compute(n9, n10, n11), NeuronNetwork.get(14).compute(n9, n10, n11));
     }
-}
-/*
-Inputs:  0  1   2
-        | \ | / |
-Hidden 1:3   4   5
-        | \ | / |
-Hidden 2:6   7   8
-        | \ | /
-Hidden 3:9 10
-        \  |
-Output:    11
+    public void train(List<List<Double>> values, List<List<Double>> answers) {
+        Double bestLossEpoch = null;
+        for (int i = 0; i < 1000; i++) {
+            Neuron epochNeuron = NeuronNetwork.get(i % NeuronNetwork.size());
+            epochNeuron.mutate();
 
- */
+            List<List<Double>> predictions = new ArrayList<>();
+            for (List<Double> value : values) {
+                predictions.add(this.predict(
+                        value.get(0),
+                        value.get(1),
+                        value.get(2)
+                ));
+            }
+
+            double currentEpochLoss = meanSquareLoss(answers, predictions);
+            if (bestLossEpoch == null || currentEpochLoss < bestLossEpoch) {
+                bestLossEpoch = currentEpochLoss;
+                epochNeuron.Remember();
+            } else {
+                epochNeuron.forget();
+            }
+        }
+    }
+
+    public static Double meanSquareLoss(List<List<Double>> correctAnswers, List<List<Double>> predictedAnswers) {
+        double sumSquare = 0;
+        int totalCount = 0;
+        for (int i = 0; i < correctAnswers.size(); i++) {
+            for (int j = 0; j < correctAnswers.get(i).size(); j++) {
+                double error = correctAnswers.get(i).get(j) - predictedAnswers.get(i).get(j);
+                sumSquare += (error * error);
+                totalCount++;
+            }
+        }
+        return sumSquare / totalCount;
+    }
+
+}
